febddf7d1cec274e1b6a10abb8fef12e415bd9c1
Mansoor <beingdeveloper96@gmail.com>
2023-06-02 19:57:42 +0500

    Setup social-wall front-end component

--- src/FilamentSocialWallServiceProvider.php
+++ src/FilamentSocialWallServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Mansoor\FilamentSocialWall;
 
+use Livewire\Livewire;
 use Filament\PluginServiceProvider;
+use Mansoor\FilamentSocialWall\Http\Livewire\SocialWall;
 use Spatie\LaravelPackageTools\Package;
 
 class FilamentSocialWallServiceProvider extends PluginServiceProvider
@@ -25,10 +27,12 @@ class FilamentSocialWallServiceProvider extends PluginServiceProvider
         'plugin-filament-social-wall' => __DIR__ . '/../resources/dist/filament-social-wall.css',
     ];
 
+    // TODO: remove/cleanup at the end if not required
     // protected array $scripts = [
     //     'plugin-filament-social-wall' => __DIR__ . '/../resources/dist/filament-social-wall.js',
     // ];
 
+    // TODO: remove/cleanup at the end if not required
     // protected array $beforeCoreScripts = [
     //     'plugin-filament-social-wall' => __DIR__ . '/../resources/dist/filament-social-wall.js',
     // ];
@@ -41,5 +45,12 @@ class FilamentSocialWallServiceProvider extends PluginServiceProvider
             ->hasMigration('create_social_providers_table')
             ->hasViews('filament-social-wall')
             ->hasRoute('filament-social-wall');
+    }
+
+    public function packageBooted(): void
+    {
+        parent::packageBooted();
+
+        Livewire::component('social-wall', SocialWall::class);
     }
 }
---
+++ src/Http/Livewire/SocialWall.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mansoor\FilamentSocialWall\Http\Livewire;
+
+use Livewire\Component;
+
+class SocialWall extends Component
+{
+    public function render()
+    {
+        return view('filament-social-wall::livewire.social-wall');
+    }
+}
